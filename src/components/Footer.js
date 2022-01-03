3ed2fce30765b868c1e819dd0ee4cf8ef155b5a9
oscar-lopez-projects <d1lopeztv@gmail.com>
2022-01-02 22:16:01 -0800

    less is more

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,54 +14,27 @@ const Footer = () =>{
                 <div className="mission-title">
                     <div className="gat">
                         <h2 style={{textAlign:"left"}} >Our Mission:</h2>
-                        <p style={{textAlign:"left"}}>
+                        <p style={{textAlign:"left", width:"70%"}}>
                         I've provided an answer, but FWIW, as a programmer myself,
                         I don't know if I would call that code inert or a dummy
                         series. It sounds useful to me. Dummy code, in my mind </p>
                     </div>
-        
-                    <div className="logoz">
-                        <img className="mjc-main-logo" src={mainLogo} alt="Our Logo"/>
-                    </div>
-                        <div className="quick-links">
-                        <div className="links">
-                        <div className="quick-link-title">Quick Links:</div>
-                            <Link to="/gallery"><div className="link-letter">Gallery</div> </Link>
-                            <Link to="/order"><div className="link-letter">Order</div> </Link>
-                            <Link to="/catering"><div className="link-letter">Catering</div> </Link>
-                            <Link to="/contact-us"><div className="link-letter">Contact</div> </Link>
-                            <Link><div className="link-letter">Donations</div> </Link>
-                            <Link><div className="link-letter">Certifications</div></Link>
+                    {/* make a div to carry two links per row */}
+                            <div className="quick-links">
+                                <div className="links">
+                                    <div className="quick-link-title">Quick Links:</div>
+                                    <Link to="/gallery"><div className="link-letter">Gallery</div> </Link>
+                                    <Link to="/order"><div className="link-letter">Order</div> </Link>
+                                    <Link to="/catering"><div className="link-letter">Catering</div> </Link>
+                                    <Link to="/contact-us"><div className="link-letter">Contact</div> </Link>
+                                    <Link><div className="link-letter">Donations</div> </Link>
+                                    <Link><div className="link-letter">Certifications</div></Link>
+                                </div>
                         </div>
                 </div>
-                </div>
+    
 
-                <div className="tablet-social">
-                            <div className="social-tablet-title">Social Media</div>
-
-                            <a href="https://www.facebook.com/ModestoJelloCake/">
-                                <img className="img-logo-size" src={facebookLogo} alt="https://www.facebook.com/ModestoJelloCake/" />
-                            </a>
-                            <a href="https://www.instagram.com/modesto.jello.cakes/">
-                                <img className="img-logo-size" src={instaLogo} alt="Instagram Icon"/>
-                            </a>
-                            {/*Need to set up for when clicked, give the option of spanish or english. functionality needed */}
-                            <a href="tel:2094121586">
-                                <img className="img-logo-size" src={phoneLogo} alt="Phone Icon" />
-                            </a>
-                </div>
-              
-                {/* <div className="quick-links">
-                    <div className="links">
-                    <div className="quick-link-title">Quick Links:</div>
-                        <Link to="/gallery"><div className="link-letter">Gallery</div> </Link>
-                        <Link to="/order"><div className="link-letter">Order</div> </Link>
-                        <Link to="/catering"><div className="link-letter">Catering</div> </Link>
-                        <Link to="/contact-us"><div className="link-letter">Contact</div> </Link>
-                        <Link><div className="link-letter">Donations</div> </Link>
-                        <Link><div className="link-letter">Certifications</div></Link>
-                    </div>
-                </div> */}
+                
                 
             </div>
 
@@ -71,6 +44,7 @@ const Footer = () =>{
                 <div className="copyright">
                 Copyright©2021 All Rights Reserved By Modesto Jello Cakes
                 </div>
+                
                 <div className="social">
                             <a href="https://www.facebook.com/ModestoJelloCake/">
                                 <img className="img-logo-size" src={facebookLogo} alt="https://www.facebook.com/ModestoJelloCake/" />
@@ -78,7 +52,7 @@ const Footer = () =>{
                             <a href="https://www.instagram.com/modesto.jello.cakes/">
                                 <img className="img-logo-size" src={instaLogo} alt="Instagram Icon"/>
                             </a>
-                            {/*Need to set up for when clicked, give the option of spanish or english. functionality needed */}
+                            {/* Need to set up for when clicked, give the option of spanish or english. functionality needed */}
                             <a href="tel:2094121586">
                                 <img className="img-logo-size" src={phoneLogo} alt="Phone Icon" />
                             </a>
